3158cb631f8fa0e07b9773d3596bcfef53a3d2f5
Maame Yaa Serwaa Bona-Mensa <mbonamensa@gmail.com>
2022-10-10 18:48:04 +0000

    remove unwanted whitespaces

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const body = document.body
 const themeBtn = document.querySelector("#theme-btn")
 
+// TOGGLE LIGHT OR DARKMODE
 themeBtn.addEventListener("click", () => {
     if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
         body.classList.toggle("lightmode")
